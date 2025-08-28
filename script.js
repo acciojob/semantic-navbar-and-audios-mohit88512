c0b0856591017938aecb24da264fdda6625592fb
mohit88512 <98802810+mohit88512@users.noreply.github.com>
2025-08-28 13:29:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+const root = [
+      {
+        id: 1,
+        title: "The Neverwritten Role Playing Game",
+        artist: "Kangaroo MusiQue",
+        src: "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3",
+        type: "mp3"
+      },
+      {
+        id: 2,
+        title: "Lepidoptera",
+        artist: "Epoq",
+        src: "http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg",
+        type: "ogg"
+      }
+    ];
+
+    console.log(root); // shows 2 items → each with 5 keys
